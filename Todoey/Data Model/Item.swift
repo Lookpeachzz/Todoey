fdd777a306f5a82e3b9307e987bbb57930078f34
Bunyisa Phansamrit <bunyisa@macmini-vis-mac-mini.local>
2018-07-10 11:17:40 +0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Bunyisa Phansamrit on 10/7/2561 BE.
+//  Copyright © 2561 Bunyisa Phansamrit. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
